521c337e847151d815586ab7737e3e72b0eb6a08
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-11-21 13:51:56 -0500

    Completed the projects, potentials are some validations and input clearing

--- Angular-small-projects/src/app/grocery-store/grocery-store.component.ts
+++ Angular-small-projects/src/app/grocery-store/grocery-store.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-grocery-store',
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   templateUrl: './grocery-store.component.html',
   styleUrl: './grocery-store.component.css',
 })
-export class GroceryStoreComponent {
+export class GroceryStoreComponent implements OnInit {
   productList = [
     'Cooking oil-(120)',
     'Rice-(60)',
@@ -19,20 +19,60 @@ export class GroceryStoreComponent {
     'Dry Fruits Mix-(850)',
   ];
 
+  ngOnInit(): void {
+    this.totalPrice = '00.00';
+  }
+
   addedItemList: any[] = [];
 
   currentItemName: string = '';
   itemQty: number = 0;
+  totalPrice: any = 0;
 
   getItemsInformation(itemName: string, qty: string) {
     this.currentItemName = itemName;
     this.itemQty = Number(qty);
 
-    const itemInfo = {
-      itemName: this.currentItemName,
-      qty: this.itemQty,
-    };
+    const item = this.addedItemList.find(
+      (item) => item.itemName === this.currentItemName
+    );
 
-    this.addedItemList.push(itemInfo);
+    if (item !== undefined) {
+      item.qty += this.itemQty;
+    } else {
+      const itemInfo = {
+        itemName: this.currentItemName,
+        price: this.getItemPrice(this.currentItemName),
+        qty: this.itemQty,
+      };
+
+      this.addedItemList.push(itemInfo);
+    }
+    this.calculateTotalPrice();
+  }
+
+  getItemPrice(item: string): number {
+    const startIndex = item.indexOf('(') + 1;
+    const endIndex = item.indexOf(')');
+    const number = parseInt(item.substring(startIndex, endIndex), 10);
+    return number;
+  }
+
+  calculatePrice(item: any) {
+    return item.price + ' * ' + item.qty + ' = ' + item.price * item.qty;
+  }
+  calculateTotalPrice() {
+    this.totalPrice = this.addedItemList.reduce(
+      (accumulator, currentValue) =>
+        accumulator + currentValue.price * currentValue.qty,
+      0
+    );
+  }
+
+  deleteItem(item: any) {
+    this.addedItemList = this.addedItemList.filter(
+      (products) => products.itemName !== item.itemName
+    );
+    this.calculateTotalPrice();
   }
 }
