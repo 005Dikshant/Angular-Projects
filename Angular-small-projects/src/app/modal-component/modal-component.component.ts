bf9645ef9fc6fdc4aa3ada6b5f2a90be530fa8c9
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-12-04 17:30:20 -0500

    Getting the native html element
    keytake aways:
    
    - @ViewChild('model2') model2: ElementRef | undefined;
    - #model2

--- Angular-small-projects/src/app/modal-component/modal-component.component.ts
+++ Angular-small-projects/src/app/modal-component/modal-component.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-modal-component',
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './modal-component.component.css',
 })
 export class ModalComponentComponent {
+  @ViewChild('model2') model2: ElementRef | undefined;
+
   openByDocument() {
     const modal = document.getElementById('Modal1');
     if (modal) {
@@ -19,6 +21,17 @@ export class ModalComponentComponent {
     const modal = document.getElementById('Modal1');
     if (modal) {
       modal.style.display = 'none';
+    }
+  }
+
+  openByViewChild() {
+    if (this.model2) {
+      this.model2.nativeElement.style.display = 'block';
+    }
+  }
+  closeByViewChild() {
+    if (this.model2) {
+      this.model2.nativeElement.style.display = 'none';
     }
   }
 }
